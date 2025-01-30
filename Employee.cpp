c4a490dde4f5bc18d99b99ac4108d9b308bff92e
zwolinskirj <zwolinskirj@sunybroome.edu>
2025-01-30 10:56:04 -0500

    Updating the Employee

--- Employee.cpp
+++ Employee.cpp
@@ -1,1 +1,28 @@
 #include "Employee.h"
+
+
+Employee::Employee(const std::string& n, const std::string& e) {
+	name = n;
+	email = e;
+	location = "Not Set";
+	skills = new skill[5];
+}
+
+Employee::Employee(const std::string& n, const std::string& e, const std::string& l) {
+	name = n;
+	email = e;
+	location = l;
+	skills = new skill[5];
+}
+
+std::string Employee::getName() const { return name; }
+std::string Employee::getEmail() const { return email; }
+std::string Employee::getLocation() const { return location; }
+
+void learn(std::string skill_name) {
+
+}
+std::ostream& operator<<(std::ostream& os, const Employee& e) {
+	os << "Employee: " << e.name << " - " << e.email << " - " << e.location << std::endl;
+	return os;
+}
--- Employee.h
+++ Employee.h
@@ -1,5 +1,35 @@
 #pragma once
+#include <string>
+#include <iostream>
+#include <array>
+#include <iterator>
+
 class Employee
 {
+private:
+	std::string name;
+	std::string email;
+	std::string location;
+	
+	struct skill {
+		std::string skill_name;
+		int skill_level;
+	};
+
+	skill* skills;
+	
+
+public:
+	Employee(const std::string& n, const std::string& e);
+	Employee(const std::string& n, const std::string& e, const std::string &l);
+	std::string getName() const;
+	std::string getEmail() const;
+	std::string getLocation() const;
+
+	void learn(std::string);
+
+	friend std::ostream& operator<<(std::ostream& os, const Employee& e);
+
+
 };
 
